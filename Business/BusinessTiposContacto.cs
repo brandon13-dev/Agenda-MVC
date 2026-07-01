dd2d9d71bca45f5d4034f371f3d8c3bf781e793b
Brandon Arroyo <brandon_arroyo@hotmail.es>
2026-07-01 17:08:12 -0600

    Agregar Capa de Negocios para Tipos de Contacto, sin validaciones

---
+++ Business/BusinessTiposContacto.cs
@@ -0,0 +1,20 @@
+﻿using Data;
+using Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class BusinessTiposContacto
+    {
+        DataTiposContacto _data = new DataTiposContacto();
+
+        public List<TiposContactoDTO> ObtenerTiposDeContacto()
+        {
+            return _data.ObtenerTiposContacto();
+        }
+    }
+}
