ca42749cb87b3bfbbd56188b871eda9010272c01
Brandon Arroyo <brandon_arroyo@hotmail.es>
2026-07-01 17:47:52 -0600

    Agregar Controlador para los Medios de Contacto y una vista preliminar para la vista de Medios de Contacto

---
+++ Agenda-Web/Controllers/MediosContactoController.cs
@@ -0,0 +1,41 @@
+﻿using Business;
+using Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Agenda_Web.Controllers
+{
+    public class MediosContactoController : Controller
+    {
+        BusinessContactos _businessContacto = new BusinessContactos();
+        BusinessMediosContacto _businessMedios = new BusinessMediosContacto();
+        BusinessTiposContacto _businessTipos = new BusinessTiposContacto();
+
+        public ActionResult MediosContacto(int contactoId)
+        {
+            if (Session["Usuario"] == null) return RedirectToAction("Login", "Login");
+
+            ContactoDTO contactoDTO = _businessContacto.ObtenerContactoPorId(contactoId);
+            contactoDTO.ContactoMedios = _businessMedios.ObtenerMediosPorContacto(contactoId);
+
+            ViewBag.TiposContacto = _businessTipos.ObtenerTiposDeContacto().ToList();
+
+            return View("MediosContacto", contactoDTO);
+        }
+
+        public ActionResult AgregarMedio(ContactoMediosDTO medio)
+        {
+            _businessMedios.AgregarMedioContacto(medio);
+            return RedirectToAction("MediosContacto", new { contactoId = medio.ContactoId });
+        }
+
+        public ActionResult EliminarMedio(int contactoMedioId, int contactoId)
+        {
+            _businessMedios.EliminarMedioContacto(contactoMedioId);
+            return RedirectToAction("MediosContacto", new { contactoId = contactoId });
+        }
+    }
+}
