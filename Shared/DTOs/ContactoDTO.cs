0be997d06124b102f9cada9b72cc31dcc98b4c77
Brandon Arroyo <brandon_arroyo@hotmail.es>
2026-07-01 17:55:38 -0600

    Agregar DTO para Contactos

---
+++ Shared/DTOs/ContactoDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs
+{
+    public class ContactoDTO
+    {
+        public int ContactoId { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public string ExtensionImagen { get; set; }
+        public int UsuarioId { get; set; }
+        public string CorreoElectronico { get; set; }
+        public List<ContactoMediosDTO> ContactoMedios { get; set; }
+    }
+}
