f1cfb1fc4646b938db9bbbdbb654d2c81484b4d1
Brandon Arroyo <brandon_arroyo@hotmail.es>
2026-07-01 18:12:07 -0600

    Agregar Controlador para Login

---
+++ Agenda-Web/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Agenda_Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Business
+        BusinessLogin _business = new BusinessLogin();
+        // Vista principal para login
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+        public ActionResult ObtenerLogin(string usuario, string password)
+        {
+            try
+            {
+                Session["Usuario"] = _business.ObtenerLogin(usuario, password);
+                TempData["success"] = "Usuario logeado correctamente";
+                return RedirectToAction("Contactos", "Contactos");
+            }
+            catch (ArgumentException ex)
+            {
+                TempData["error"] = ex.Message;
+                return RedirectToAction("Login");
+            }
+            catch (Exception)
+            {
+                TempData["error"] = "Ocurrio un error en el servidor";
+                return RedirectToAction("Error");
+            }
+        }
+
+        public ActionResult CerrarSesion()
+        {
+            Session["Usuario"] = null;
+            return RedirectToAction("Login");
+        }
+    }
+}
